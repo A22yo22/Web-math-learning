415e5d0b1d1cc4de050342630c0f9e5456129a60
A22yo22 <a22yo22io@gmail.com>
2022-06-30 00:02:12 +0200

    jup

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let inputs = document.getElementById("inputs");
+
+let firstMultiplyer = 1;
+let secondMultiplyer = 1;
+
+let whatsToCalculate = document.createTextNode(`Was ist ${firstMultiplyer}  mal ${secondMultiplyer}?`);
+
+inputs.appendChild(whatsToCalculate);
